4902b4a723d3681ca7adba10e4a1e32b4c954a8b
Hoang Phan <phan.minhhoang2606@gmail.com>
2019-02-19 16:06:15 -0500

    Fix minor errors with read (#5)
    * Fix minor errors
    
    * Add version

--- scripts/util.py
+++ scripts/util.py
@@ -4,10 +4,6 @@ import os
 from pathlib2 import Path
 
 
-def refined(str):
-    return str.replace('\"', '').replace('\n', '')
-
-
 def split_array(arr, condition):
     if len(arr) == 0:
         return []
@@ -24,26 +20,13 @@ def split_array(arr, condition):
 
 
 def read_file(file_name, is_train=True):
-    with open(file_name) as fp:
-        data_lines = filter(lambda x: x != '', fp.read().split('\n'))
-        pattern = 'train' if is_train else 'test'
-        datas = split_array(data_lines, lambda x: pattern in x)
-        if is_train:
-            result_array = map(
-                lambda x: [x[0], ' '.join(x[1:-1]), int(x[-1])], datas)
-        else:
-            result_array = map(
-                lambda x: [x[0], ' '.join(x[1:-1])], datas
-            )
-        columns = ['name', 'text', 'label'] if is_train else [
-            'name', 'text'
-        ]
-        return pd.DataFrame(result_array, columns=columns)
-
-
-path = os.getcwd()
-if path.endswith('scripts'):
-    path = os.path.abspath('..')
-train_path = os.path.join(path, 'data/test.crash')
-print(read_file(train_path, False)
-      ['name'].describe())
+    data_lines = filter(lambda x: x != '', open(file_name).read().split('\n'))
+    pattern = 'train' if is_train else 'test'
+    datas = split_array(data_lines, lambda x: pattern in x)
+    if is_train:
+        result_array = map(
+            lambda x: [x[0], ' '.join(x[1:-1]), int(x[-1])], datas)
+    else:
+        result_array = map(lambda x: [x[0], ' '.join(x[1:-1])], datas)
+    columns = ['name', 'text', 'label'] if is_train else ['name', 'text']
+    return pd.DataFrame(result_array, columns=columns)
