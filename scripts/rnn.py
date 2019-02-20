03cb86a73e5ecb3006a6cc233c27b1ddeef874cd
Nhat Pham <hoangnhat123456@live.com>
2019-02-19 23:11:37 -0800

    Use CuDNNGRU instead of GRU.

--- scripts/rnn.py
+++ scripts/rnn.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import keras
 from keras.models import Model
-from keras.layers import Dense, Embedding, Input, LSTM, GRU, Bidirectional, GlobalMaxPool1D, Dropout
+from keras.layers import Dense, Embedding, Input, CuDNNGRU, GRU, Bidirectional, GlobalMaxPool1D, Dropout
 from keras.preprocessing import text, sequence
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import initializers, regularizers, constraints, optimizers, layers
@@ -12,9 +12,9 @@ import keras.backend as K
 def RNNKeras(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxlen = 100):
     inp = Input(shape = (maxlen, ))
     x = Embedding(input_dim = max_features, output_dim = embed_size, weights = [embeddingMatrix])(inp)
-    x = Bidirectional(GRU(50, return_sequences = True))(x)
+    x = Bidirectional(CuDNNGRU(50, return_sequences = True))(x)
     # x = Dropout(0.1)(x)
-    x = Bidirectional(GRU(50, return_sequences = True))(x)
+    x = Bidirectional(CuDNNGRU(50, return_sequences = True))(x)
     x = Dropout(0.1)(x)
     x = GlobalMaxPool1D()(x)
     x = Dropout(0.1)(x)
