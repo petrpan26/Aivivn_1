8cf9f87daeea7a5b294cdbf59c4bd1a46f80d3a3
Nhat Pham <hoangnhat123456@live.com>
2019-02-21 21:37:29 -0800

    Fix some bugs.

--- scripts/util.py
+++ scripts/util.py
@@ -12,6 +12,8 @@ from spacy.lang.vi import Vietnamese
 from spacy.attrs import ORTH
 from .constant import EMOTICONS, DEFAULT_MAX_FEATURES, DEFAULT_MAX_LENGTH
 from gensim.models.keyedvectors import KeyedVectors
+from sklearn.metrics import f1_score
+
 
 
 def split_array(arr, condition):
@@ -104,3 +106,17 @@ def predictions_to_submission(test_data, predictor):
     submission = test_data[['id']]
     submission['label'] = test_data['text'].progress_apply(predictor)
     return submission
+
+def find_threshold(pred_proba, y_true, metric = f1_score):
+    cur_acc = 0
+    cur_thres = 0
+    for ind in range(pred_proba.shape[0] - 1):
+        threshold = (pred_proba[ind][0] + pred_proba[ind + 1][0]) / 2
+        pred = (pred_proba > threshold).astype(np.int8)
+        acc = metric(pred, y_true)
+        if acc > cur_acc:
+            cur_thres = threshold
+            cur_acc = acc
+
+    return cur_thres
+
