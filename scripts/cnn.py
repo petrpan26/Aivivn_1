14c0f2feeba5a93eebc6bd3561f415c02d28f244
Nhat Pham <hoangnhat123456@live.com>
2019-03-08 15:45:03 -0800

    Implement LSTMCNN.

--- scripts/cnn.py
+++ scripts/cnn.py
@@ -3,6 +3,7 @@ from keras.layers import \
     Dense, Embedding, Input, \
     Conv1D, MaxPool1D, \
     Dropout, BatchNormalization, \
+    Bidirectional, CuDNNLSTM, \
     Concatenate, Flatten
 from .util import f1
 
@@ -37,6 +38,46 @@ def TextCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxl
 
 
     flat = Flatten()(conv_3)
+
+    op = Dense(64, activation = "relu")(flat)
+    # op = Dropout(0.5)(op)
+    op = BatchNormalization()(op)
+    op = Dense(1, activation = "sigmoid")(op)
+
+    model = Model(inputs = inp, outputs = op)
+    model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy', f1])
+    return model
+
+
+
+def LSTMCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxlen = 100, filter_sizes = {2, 3, 4}):
+    inp = Input(shape = (maxlen, ))
+    x = Embedding(input_dim = max_features, output_dim = embed_size, weights = [embeddingMatrix])(inp)
+    x = Bidirectional(CuDNNLSTM(128, return_sequences = True))(x)
+
+    conv_ops = []
+    for filter_size in filter_sizes:
+        conv = Conv1D(128, filter_size, activation = 'relu')(x)
+        pool = MaxPool1D(5)(conv)
+        conv_ops.append(pool)
+
+    concat = Concatenate(axis = 1)(conv_ops)
+    # concat = Dropout(0.1)(concat)
+    concat = BatchNormalization()(concat)
+
+
+    conv_2 = Conv1D(128, 5, activation = 'relu')(concat)
+    conv_2 = MaxPool1D(5)(conv_2)
+    conv_2 = BatchNormalization()(conv_2)
+    # conv_2 = Dropout(0.1)(conv_2)
+
+    # conv_3 = Conv1D(128, 5, activation = 'relu')(conv_2)
+    # conv_3 = MaxPool1D(5)(conv_3)
+    # conv_3 = BatchNormalization()(conv_3)
+    # conv_3 = Dropout(0.1)(conv_3)
+
+
+    flat = Flatten()(conv_2)
 
     op = Dense(64, activation = "relu")(flat)
     # op = Dropout(0.5)(op)
