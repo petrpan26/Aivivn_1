1534c149e93a0118c1ab90cd44a1b33e11edf3cd
Nhat Pham <hoangnhat123456@live.com>
2019-03-08 15:59:39 -0800

    Increase filter numbers.

--- scripts/cnn.py
+++ scripts/cnn.py
@@ -11,7 +11,7 @@ from .util import f1
 
 # Based on https://richliao.github.io/supervised/classification/2016/11/26/textclassifier-convolutional/
 # https://www.aclweb.org/anthology/D14-1181
-def TextCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxlen = 100, filter_sizes = {2, 3, 4}):
+def TextCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxlen = 100, filter_sizes = {2, 3, 4, 5}):
     inp = Input(shape = (maxlen, ))
     x = Embedding(input_dim = max_features, output_dim = embed_size, weights = [embeddingMatrix])(inp)
 
@@ -49,11 +49,12 @@ def TextCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxl
     return model
 
 
-
-def LSTMCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxlen = 100, filter_sizes = {2, 3, 4}):
+# Based on http://konukoii.com/blog/2018/02/19/twitter-sentiment-analysis-using-combined-lstm-cnn-models/
+def LSTMCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxlen = 100, filter_sizes = {2, 3, 4, 5}):
     inp = Input(shape = (maxlen, ))
     x = Embedding(input_dim = max_features, output_dim = embed_size, weights = [embeddingMatrix])(inp)
     x = Bidirectional(CuDNNLSTM(128, return_sequences = True))(x)
+    x = Dropout(0.5)(x)
 
     conv_ops = []
     for filter_size in filter_sizes:
@@ -62,14 +63,14 @@ def LSTMCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxl
         conv_ops.append(pool)
 
     concat = Concatenate(axis = 1)(conv_ops)
-    # concat = Dropout(0.1)(concat)
-    concat = BatchNormalization()(concat)
+    concat = Dropout(0.5)(concat)
+    # concat = BatchNormalization()(concat)
 
 
     conv_2 = Conv1D(128, 5, activation = 'relu')(concat)
     conv_2 = MaxPool1D(5)(conv_2)
-    conv_2 = BatchNormalization()(conv_2)
-    # conv_2 = Dropout(0.1)(conv_2)
+    # conv_2 = BatchNormalization()(conv_2)
+    conv_2 = Dropout(0.5)(conv_2)
 
     # conv_3 = Conv1D(128, 5, activation = 'relu')(conv_2)
     # conv_3 = MaxPool1D(5)(conv_3)
@@ -80,7 +81,7 @@ def LSTMCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxl
     flat = Flatten()(conv_2)
 
     op = Dense(64, activation = "relu")(flat)
-    # op = Dropout(0.5)(op)
+    op = Dropout(0.5)(op)
     op = BatchNormalization()(op)
     op = Dense(1, activation = "sigmoid")(op)
 
