603e62609af1d6fcfa3226cb4b31c3888abbfcd1
Nhat Pham <hoangnhat123456@live.com>
2019-03-09 13:53:12 -0800

    Implement CNN with residual connections.

--- scripts/cnn.py
+++ scripts/cnn.py
@@ -4,7 +4,7 @@ from keras.layers import \
     Conv1D, MaxPool1D, \
     Dropout, BatchNormalization, \
     Bidirectional, CuDNNLSTM, \
-    Concatenate, Flatten
+    Concatenate, Flatten, Add
 from .util import f1
 
 
@@ -47,6 +47,53 @@ def TextCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxl
     model = Model(inputs = inp, outputs = op)
     model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy', f1])
     return model
+
+
+def VDCNN(embeddingMatrix = None, embed_size = 400, max_features = 20000, maxlen = 100, filter_sizes = {2, 3, 4, 5}):
+    inp = Input(shape = (maxlen, ))
+    x = Embedding(input_dim = max_features, output_dim = embed_size, weights = [embeddingMatrix])(inp)
+
+    conv_ops = []
+    for filter_size in filter_sizes:
+        conv = Conv1D(128, filter_size, activation = 'relu')(x)
+        pool = MaxPool1D(5)(conv)
+        conv_ops.append(pool)
+
+    concat = Concatenate(axis = 1)(conv_ops)
+    # concat = Dropout(0.1)(concat)
+    concat = BatchNormalization()(concat)
+
+
+    conv_2_main = Conv1D(128, 5, activation = 'relu', padding='same')(concat)
+    conv_2_main = BatchNormalization()(conv_2_main)
+    conv_2_main = Conv1D(128, 5, activation = 'relu', padding='same')(conv_2_main)
+    conv_2_main = BatchNormalization()(conv_2_main)
+    conv_2 = Add()([concat, conv_2_main])
+    conv_2 = MaxPool1D(5)(conv_2)
+    # conv_2 = BatchNormalization()(conv_2)
+    # conv_2 = Dropout(0.1)(conv_2)
+
+    conv_3_main = Conv1D(128, 5, activation = 'relu', padding='same')(conv_2)
+    conv_3_main = BatchNormalization()(conv_3_main)
+    conv_3_main = Conv1D(128, 5, activation = 'relu', padding='same')(conv_3_main)
+    conv_3_main = BatchNormalization()(conv_3_main)
+    conv_3 = Add()([conv_2, conv_3_main])
+    conv_3 = MaxPool1D(5)(conv_3)
+    # conv_3 = BatchNormalization()(conv_3)
+    # conv_3 = Dropout(0.1)(conv_3)
+
+
+    flat = Flatten()(conv_3)
+
+    op = Dense(64, activation = "relu")(flat)
+    # op = Dropout(0.5)(op)
+    op = BatchNormalization()(op)
+    op = Dense(1, activation = "sigmoid")(op)
+
+    model = Model(inputs = inp, outputs = op)
+    model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy', f1])
+    return model
+
 
 
 # Based on http://konukoii.com/blog/2018/02/19/twitter-sentiment-analysis-using-combined-lstm-cnn-models/
